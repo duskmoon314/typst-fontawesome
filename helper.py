de4f09f8dc4197f70330b9c9cecc84e130d2c6a2
Campbell He <kp.campbell.he@duskmoon314.com>
2024-07-23 10:33:41 +0800

    feat: add pro fonts to default list
    merge two helper scripts

--- helper.py
+++ helper.py
@@ -9,13 +9,6 @@ import textwrap
 
 parser = argparse.ArgumentParser(description="typst-fontawesome helper script")
 
-parser.add_argument(
-    "-d",
-    "--download",
-    help="Download FontAwesome fonts and metadata (occur two times to extract the zip file)",
-    action="count",
-    default=0,
-)
 parser.add_argument("-v", "--version", help="FontAwesome version", required=True)
 parser.add_argument(
     "-o", "--output", help="Output dir (default: current dir .)", default="."
@@ -27,30 +20,38 @@ parser.add_argument(
     default=["lib", "doc"],
 )
 
-
-def download(version, output, extract=False):
-    ZIP_LINK_TEMPLATE = (
-        "https://use.fontawesome.com/releases/v{}/fontawesome-free-{}-desktop.zip"
-    )
-    zip_link = ZIP_LINK_TEMPLATE.format(version, version)
-
-    print(f"Downloading FontAwesome {version} metadata from {zip_link}")
-
-    zip_file = os.path.join(output, "fontawesome.zip")
-
-    # Download the zip file to the output directory
-    req = urllib.request.Request(zip_link, headers={"User-Agent": "Mozilla/5.0"})
-    with urllib.request.urlopen(req) as response:
-        with open(zip_file, "wb") as f:
-            shutil.copyfileobj(response, f)
-
-    if extract:
-        with zipfile.ZipFile(zip_file, "r") as zip_ref:
-            zip_ref.extractall(output)
+API_URL = "https://api.fontawesome.com"
+QUERY_TEMPLATE = """
+query {{
+    release (version: "{version}") {{
+        icons {{
+            id,
+            unicode,
+            familyStylesByLicense {{
+                free {{ style }},
+                pro {{ style }}
+            }},
+            aliases {{ names }}
+        }}
+    }}
+}}
+"""
 
 
 def generate_lib(version, output):
     print(f"Generating typst lib for FontAwesome {version}")
+
+    req = urllib.request.Request(
+        API_URL,
+        headers={"Content-Type": "application/json", "User-Agent": "Mozilla/5.0"},
+        data=json.dumps({"query": QUERY_TEMPLATE.format(version=version)}).encode(
+            "utf-8"
+        ),
+    )
+
+    data = {}
+    with urllib.request.urlopen(req) as response:
+        data = json.load(response)
 
     LIB_PREAMBLE_TEMPLATE = """\
     #import "lib-impl.typ": fa-icon
@@ -61,51 +62,25 @@ def generate_lib(version, output):
     """
 
     lib_preamble = textwrap.dedent(LIB_PREAMBLE_TEMPLATE).format(version=version)
-
     lib_file = os.path.join(output, "lib-gen.typ")
+
+    icon_func_str = ""
 
     with open(lib_file, "w") as f:
         f.write(lib_preamble)
 
-        # Find the metadata/icons.json file with glob
-        icons_file = glob.glob(
-            os.path.join(output, "**/metadata/icons.json"), recursive=True
-        )
-        if len(icons_file) == 0:
-            raise Exception("Cannot find metadata/icons.json")
-        icons_file = icons_file[0]
+        for icon in data["data"]["release"]["icons"]:
+            f.write(f'  "{icon["id"]}": "\\u{{{icon["unicode"]}}}",\n')
+            icon_func_str += (
+                f'#let fa-{icon["id"]} = fa-icon.with("\\u{{{icon["unicode"]}}}")\n'
+            )
 
-        icon_func_str = ""
-
-        with open(icons_file, "r") as icons_f:
-            icons_data = json.load(icons_f)
-
-            for icon_name, icon_data in icons_data.items():
-                # Check whether the icon only support solid style
-                # styles contains "solid" but no "regular"
-                solid = False
-                if (
-                    "solid" in icon_data["styles"]
-                    and "regular" not in icon_data["styles"]
-                ):
-                    solid = True
-
-                # Generate the icon line
-                f.write(f'  "{icon_name}": "\\u{{{icon_data["unicode"]}}}",\n')
-                icon_func_str += (
-                    f'#let fa-{icon_name} = fa-icon.with("\\u{{{icon_data["unicode"]}}}")\n'
-                    if not solid
-                    else f'#let fa-{icon_name} = fa-icon.with("\\u{{{icon_data["unicode"]}}}", solid: true)\n'
-                )
-
-                # Generate the alias lines
-                if "aliases" in icon_data:
-                    if "names" in icon_data["aliases"]:
-                        for alias_name in icon_data["aliases"]["names"]:
-                            f.write(
-                                f'  "{alias_name}": "\\u{{{icon_data["unicode"]}}}",\n'
-                            )
-                            icon_func_str += f"#let fa-{alias_name} = fa-icon.with(\"\\u{{{icon_data['unicode']}}}\")\n"
+            if icon["aliases"]:
+                for alias in icon["aliases"]["names"]:
+                    f.write(f'  "{alias}": "\\u{{{icon["unicode"]}}}",\n')
+                    icon_func_str += (
+                        f'#let fa-{alias} = fa-icon.with("\\u{{{icon["unicode"]}}}")\n'
+                    )
 
         f.write(")\n")
         f.write(icon_func_str)
@@ -114,51 +89,44 @@ def generate_lib(version, output):
 def generate_gallery(version, output):
     print(f"Generating typst gallery for FontAwesome {version}")
 
+    req = urllib.request.Request(
+        API_URL,
+        headers={"Content-Type": "application/json", "User-Agent": "Mozilla/5.0"},
+        data=json.dumps({"query": QUERY_TEMPLATE.format(version=version)}).encode(
+            "utf-8"
+        ),
+    )
+
+    data = {}
+    with urllib.request.urlopen(req) as response:
+        data = json.load(response)
+
     gallery_file = os.path.join(output, "gallery.typ")
 
     with open(gallery_file, "w") as f:
         f.write('#import "lib.typ": *\n')
-
-        # Find the metadata/icons.json file with glob
-        icons_file = glob.glob(
-            os.path.join(output, "**/metadata/icons.json"), recursive=True
-        )
-        if len(icons_file) == 0:
-            raise Exception("Cannot find metadata/icons.json")
-        icons_file = icons_file[0]
 
         f.write(
             # "#grid(columns: (20em, 3em, 3em, 3em), [typst code], [default], [solid], [`fa-icon` with text])\n"
             "#table(columns: (3fr, 1fr, 1fr, 2fr), stroke: none, table.header([typst code], [default], [solid], [`fa-icon` with text]),\n"
         )
 
-        with open(icons_file, "r") as icons_f:
-            icons_data = json.load(icons_f)
+        for icon in data["data"]["release"]["icons"]:
+            f.write(
+                f'```typst #fa-{icon["id"]}()```, fa-{icon["id"]}(), fa-{icon["id"]}(solid: true), fa-icon("{icon["id"]}"),\n'
+            )
 
-            for icon_name, icon_data in icons_data.items():
-                # Generate the icon line
-                f.write(
-                    # f'#grid(columns: (20em, 3em, 3em, 3em), ```typst #fa-{icon_name}()```, fa-{icon_name}(), fa-{icon_name}(solid: true)), fa-icon("{icon_name}")\n'
-                    f'```typst #fa-{icon_name}()```, fa-{icon_name}(), fa-{icon_name}(solid: true), fa-icon("{icon_name}"),\n'
-                )
-
-                # Generate the alias lines
-                if "aliases" in icon_data:
-                    if "names" in icon_data["aliases"]:
-                        for alias_name in icon_data["aliases"]["names"]:
-                            f.write(
-                                # f'#grid(columns: (20em, 3em, 3em, 3em), ```typst #fa-{alias_name}()```, fa-{alias_name}(), fa-{alias_name}(solid: true)), fa-icon("{alias_name}")\n'
-                                f'```typst #fa-{alias_name}()```, fa-{alias_name}(), fa-{alias_name}(solid: true), fa-icon("{alias_name}"),\n'
-                            )
+            if icon["aliases"]:
+                for alias in icon["aliases"]["names"]:
+                    f.write(
+                        f'```typst #fa-{alias}()```, fa-{alias}(), fa-{alias}(solid: true), fa-icon("{alias}"),\n'
+                    )
 
         f.write(")")
 
 
 def main():
     args = parser.parse_args()
-
-    if args.download > 0:
-        download(args.version, args.output, args.download > 1)
 
     if "lib" in args.generate:
         generate_lib(args.version, args.output)
